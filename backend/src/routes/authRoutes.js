dd70a16f0c3956c74fb67814ecd6db7a6e5e48d2
dumpling <024dsun@gmail.com>
2025-11-07 00:29:08 -0800

    user login auth (backend)

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,9 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-// We'll create these controller functions in the next step
-// For now, we'll just import them.
-import { registerUser } from '../controllers/authController.js';
+// Import controller functions from controller
+import { registerUser, loginUser } from '../controllers/authController.js';
 
 // @route   POST /api/auth/register
 // @desc    Register a new user
@@ -12,18 +11,6 @@ router.post('/register', registerUser);
 
 // @route   POST /api/auth/login
 // @desc    Log in a user and get a token
-// router.post('/login', loginUser);
-
-
-// --- TEMPORARY TEST ROUTES ---
-// Let's add simple test routes so we know the file is working
-
-// router.post('/register', (req, res) => {
-//   res.json({ message: 'Register route hit!' });
-// });
-
-router.post('/login', (req, res) => {
-  res.json({ message: 'Login route hit!' });
-});
+router.post('/login', loginUser);
 
 export default router;
