7a258103cdcd546b3d8106a9b334b9f1c2db7406
Alex <alextgiang@g.ucla.edu>
2025-11-28 17:17:08 -0800

    add missing import of useRef

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { BrowserRouter, Routes, Route, Link, useNavigate, useLocation } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
