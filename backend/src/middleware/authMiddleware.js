6a9c228436a340f4224247b2ad30a00c0e1b399b
dumpling <024dsun@gmail.com>
2025-12-03 12:25:02 -0800

    refactored authMiddleware.js

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -1,28 +1,22 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
-dotenv.config(); // Make sure it can read the JWT_SECRET
+dotenv.config();
 
+// middleware to protect routes
 const protect = (req, res, next) => {
   let token;
 
-  // 1. Check if the 'Authorization' header exists and has the token
+  // try to obtain the token and verify token is real
+  // catch errors for token verification errors
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     try {
-      // 2. Get the token from the header (e.g., "Bearer [token]")
       token = req.headers.authorization.split(' ')[1];
-
-      // 3. Verify the token is real using our secret
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      // 4. Attach the user's info to the request object
-      // We'll use this in our "create post" controller
-      req.user = decoded; // This contains { userId, email }
-
-      // 5. Call 'next()' to pass the request to the next function
+      req.user = decoded;
       next();
-
-    } catch (error) {
+    } 
+    catch (error) {
       console.error('Token verification failed', error);
       if (error.name === 'TokenExpiredError') {
         return res.status(401).json({ message: 'Session expired. Please log in again.' });
